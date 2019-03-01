ad66e0e7235fa89c4f12ddcaf82eeda744e1d664
Matvey Kostukovsky <matvelius@gmail.com>
2019-02-28 20:17:11 -0600

    Initial Commit

---
+++ Login/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Login
+//
+//  Created by Matvey on 2/28/19.
+//  Copyright © 2019 Matvey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
