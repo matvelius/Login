140f8321eebbb5ff13caaacbb5ff008b95b27e6a
Matvey Kostukovsky <matvelius@gmail.com>
2019-03-01 16:30:35 -0600

    add login functionality, improve look

--- Login/ViewController.swift
+++ Login/ViewController.swift
@@ -9,12 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var forgotPasswordButtonOutlet: UIButton!
+    @IBOutlet weak var forgotUsernameButtonOutlet: UIButton!
+    @IBOutlet weak var username: UITextField!
+    
+    @IBAction func forgotUsernameButton(_ sender: Any) {
+        performSegue(withIdentifier: "segueFromLoginToLanding", sender: forgotUsernameButton)
+    }
+    
+    @IBAction func forgotPasswordButton(_ sender: Any) {
+        performSegue(withIdentifier: "segueFromLoginToLanding", sender: forgotPasswordButton)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        guard let sender: UIButton = sender as? UIButton else { return }
+        
+        if sender == forgotPasswordButtonOutlet {
+            segue.destination.navigationItem.title = "Forgot Password"
+        } else if sender == forgotUsernameButtonOutlet {
+            segue.destination.navigationItem.title = "Forgot Username"
+        } else {
+            segue.destination.navigationItem.title = username.text
+        }
+    
+    }
 
 }
 
