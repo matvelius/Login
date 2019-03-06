0d2c490815151f8f7cf97f948ee41b2469bf959b
Matvey Kostukovsky <matvelius@gmail.com>
2019-03-06 09:10:37 -0600

    set stack to vertical or horizontal axis, depending on the orientation on the intro screen

--- Login/FirstScreen.swift
+++ Login/FirstScreen.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class FirstScreenViewController: UIViewController {
     
+    @IBOutlet weak var stackView: UIStackView!
+    
+    @IBOutlet weak var logo: UIImageView!
+    
     @IBAction func loginButtonFirstScreen (_ sender: Any) {
     }
     
@@ -18,5 +22,26 @@ class FirstScreenViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+//        logo.layer.cornerRadius = 10
+//        logo.clipsToBounds = true
+//        logo.layer.borderWidth = 3
+//        logo.layer.borderColor = UIColor.white.cgColor
+    }
+    
+    // set stack to vertical or horizontal axis, depending on the orientation!
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        coordinator.animate(alongsideTransition: { (context) -> Void in
+            
+            let orientation = UIApplication.shared.statusBarOrientation
+            
+            if orientation.isLandscape {
+                self.stackView.axis = .horizontal
+                print("horizontal")
+            } else {
+                self.stackView.axis = .vertical
+                print("vertical")
+            }
+            
+        }, completion: nil)
     }
 }
