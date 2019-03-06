dd8df88496b744f1dcc376da6b895d6552e0adc9
Matvey Kostukovsky <matvelius@gmail.com>
2019-03-06 00:07:33 -0600

    offload input validation to a separate class / swift file

--- Login/LoginViewController.swift
+++ Login/LoginViewController.swift
@@ -17,9 +17,6 @@ class LoginViewController: UIViewController {
     
     @IBOutlet weak var passwordField: UITextField!
     
-    let userDictionary = ["Bob": "password", "Mary": "anotherpassword"]
-    
-    
     
     @IBAction func forgotUsernameButton(_ sender: Any) {
         performSegue(withIdentifier: "mySegue", sender: forgotUsernameButtonOutlet)
@@ -38,61 +35,28 @@ class LoginViewController: UIViewController {
         guard let sender: UIButton = sender as? UIButton else { return }
         
         if sender == forgotPasswordButtonOutlet {
+            
             segue.destination.navigationItem.title = "Forgot Password"
+            
         } else if sender == forgotUsernameButtonOutlet {
+            
             segue.destination.navigationItem.title = "Forgot Username"
+            
         } else {
-            checkForEmptyFields()
             
-            // store username in currentUsername variable
-            guard let currentUsername = usernameField.text else {
-                alertUser(message: "Unable to parse username")
-                return
+            // input validation
+            let myValidator = InputValidation()
+            let message = myValidator.checkForEmptyFields(usernameField: usernameField, passwordField: passwordField)
+            if let alertMessage: String = message {
+                alertUser(message: alertMessage)
             }
-            
-            // store username in currentUsername variable
-            guard let currentPassword = passwordField.text else {
-                alertUser(message: "Unable to parse password")
-                return
-            }
-            
-            // validate username
-            let usernames = [String](userDictionary.keys)
-            guard usernames.contains(currentUsername) else {
-                alertUser(message: "Invalid username")
-                return
-            }
-            
-            // validate password
-            guard userDictionary[currentUsername] == currentPassword else {
-                alertUser(message: "Invalid password")
-                return
-            }
-            
-            
             
             guard let successfulLogin = segue.destination as? SuccessfulLoginViewController else { return }
             
             successfulLogin.receive = usernameField.text
             
-
-            
             segue.destination.navigationItem.title = "You are logged in."
             
-        }
-    }
-    
-    func checkForEmptyFields() {
-        // check if username was entered
-        guard !(usernameField.text == "") else {
-            alertUser(message: "Please enter your username")
-            return
-        }
-        
-        // check if password was entered
-        guard !(passwordField.text == "") else {
-            alertUser(message: "Please enter your password")
-            return
         }
     }
     
@@ -106,6 +70,6 @@ class LoginViewController: UIViewController {
         
         present(alert, animated: true, completion: nil)
     }
-
+    
 }
 
--- Login/inputValidation.swift
+++ Login/inputValidation.swift
@@ -8,16 +8,63 @@
 
 import UIKit
 
-//func checkForEmptyFields() {
-//    // check if username was entered
-//    guard !(usernameField.text == "") else {
-//        alertUser(message: "Please enter your username")
-//        return
-//    }
-//    
-//    // check if password was entered
-//    guard !(passwordField.text == "") else {
-//        alertUser(message: "Please enter your password")
-//        return
-//    }
-//}
+class InputValidation {
+    
+    let userDictionary = ["Bob": "password", "Mary": "anotherpassword"]
+
+    func checkForEmptyFields(usernameField: UITextField, passwordField: UITextField, reEnterPasswordField: UITextField...) -> String? {
+        // check if username was entered
+        guard !(usernameField.text == "") else {
+            return "Please enter your username"
+        }
+        
+        // check if password was entered
+        guard !(passwordField.text == "") else {
+            return "Please enter your password"
+        }
+        
+        // store username in currentUsername variable
+        guard let currentUsername = usernameField.text else {
+            return "Unable to parse username"
+        }
+        
+        // store username in currentUsername variable
+        guard let currentPassword = passwordField.text else {
+            return "Unable to parse password"
+        }
+        
+        // validate username
+        let usernames = [String](userDictionary.keys)
+        guard usernames.contains(currentUsername) else {
+            return "Invalid username"
+        }
+        
+        // validate password
+        guard userDictionary[currentUsername] == currentPassword else {
+            return "Invalid password"
+            
+        }
+        
+        for extraField in reEnterPasswordField {
+            
+            // check if password was re-entered
+            guard !(extraField.text == "") else {
+                return "Please re-enter the password"
+            }
+            
+            // check if passwords match
+            guard extraField.text == passwordField.text else {
+                return "Passwords don't match!"
+            }
+            
+        }
+        
+        // if inputs are validated, return nil
+        return nil
+        
+    }
+    
+    
+    
+
+}
